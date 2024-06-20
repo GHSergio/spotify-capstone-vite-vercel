afbcbb67cb33b5b022d5476707e708e745c036bf
Ming Hsu <n209202003@gmail.com>
2024-06-20 17:24:30 +0800

    modify redirect uri

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { Login, Callback, Main } from "./pages";
 import PodcastListContext from "./contexts/PodcastListContext";
 import PrivateRoute from "./components/PrivateRoute";
 
-const basename = import.meta.env.VITE_BASENAME;
-console.log("basename:", basename);
+// const basename = import.meta.env.VITE_BASENAME;
+// console.log("basename:", basename);
 
 function App() {
   return (
     <>
       <PodcastListContext>
-        <BrowserRouter basename={basename}>
-          {/* <BrowserRouter> */}
+        {/* <BrowserRouter basename={basename}> */}
+        <BrowserRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/callback" element={<Callback />} />
